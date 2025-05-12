a9c2e9407d90a45976b75e8ff7ec80f1eafcd2b9
Harry-Skerritt <harry2.skerritt@live.uwe.ac.uk>
2025-05-12 17:24:09 +0100

    Added all the functionality minus keyboard support

--- src/ShuffleCards/ShuffleCards.jsx
+++ src/ShuffleCards/ShuffleCards.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import styles from '../ShuffleCards/ShuffleCards.module.css';
+import "../index.css"
 import {useNavigate} from "react-router-dom";
 
 function ShuffleCards() {
